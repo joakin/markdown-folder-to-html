bcbbf0323c5e7eac20cbb2539e9798dcbd2ab061
joakin <joaquin@chimeces.com>
2016-07-15 15:16:27 +0200

    1.0.0

---
+++ README.md
@@ -0,0 +1,39 @@
+# markdown-folder-to-html
+
+Simplest way zero-config to generate html docs from markdown files.
+
+Copies `docs` to `_docs` and compiles markdown files to html using
+`docs/template.html`.
+
+## Usage
+
+1. Install `npm install -D markdown-folder-to-html` (You can install globally
+   too if inclined)
+3. Add `docs` to npm scripts `{"scripts": {"docs": "markdown-folder-to-html"}}`
+2. Create `docs`
+  1. `mkdir -p docs`
+  2. Add some docs `echo "**Banana**" > docs/banana.md`
+  3. Add some docs `echo "**Apple**" > docs/index.md`
+5. 🎉 `npm run docs` and `open _docs/index.html`
+
+## Custom HTML
+
+The default HTML is extremely basic:
+
+```html
+<!doctype html>
+<html>
+<body>
+<nav>
+	<!--NAV-->
+</nav>
+<article>
+	<!--CONTENT-->
+</article>
+</body>
+</html>
+```
+
+Create your own in your docs folder `docs/template.html` to use that one
+instead. Feel free to include styles inline or CSS files (since all will be
+copied to output).
---
+++ cli.js
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const sh = require('shelljs')
+const commonmark = require('commonmark')
+
+const reader = new commonmark.Parser()
+const writer = new commonmark.HtmlRenderer()
+
+const folder = 'docs'
+const output = '_docs'
+let template = path.join(folder, 'template.html')
+if (!sh.test('-e', template)) {
+  template = path.join(__dirname, 'docs', 'template.html')
+}
+
+const tpl = sh.cat(template)
+const contentR = /<!--CONTENT-->/g
+const navR = /<!--NAV-->/g
+const mdR = /\.md$/
+
+sh.mkdir('-p', output)
+sh.rm('-rf', output + '/*')
+sh.cp('-R', folder + '/*', output)
+
+const all = sh.find(output)
+const mds = all.filter((f) => f.match(mdR))
+
+mds
+  .map((f) => [f, page(nav(f, mds), md2html(sh.cat(f)))])
+  .forEach(([f, p]) => fs.writeFileSync(mdUrl(f), p))
+
+function nav (current, files) {
+  const currentDir = path.dirname(current)
+
+  return `<ul>
+${files.map((f) => {
+  const clazz = f === current ? 'active' : ''
+  const href = mdUrl(path.relative(currentDir, f))
+  const text = removeOutput(f).replace(mdR, '')
+  return `<li><a class="${clazz}" href="${href}">${text}</a></li>`
+}).join('\n')}
+</ul>`
+}
+
+function removeOutput (s) {
+  return s.replace(output + '/', '')
+}
+
+function md2html (cnts) {
+  return writer.render(reader.parse(cnts))
+}
+
+function page (nav, content) {
+  return tpl.replace(navR, nav).replace(contentR, content)
+}
+
+function mdUrl (file) {
+  return file.replace(mdR, '.html')
+}
