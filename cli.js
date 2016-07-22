62a0eb4700f327eaaa9dfaec92c346bf7472f4a1
joakin <joaquin@chimeces.com>
2016-07-22 12:26:47 +0200

    Add cli arg to change input folder
    First positional argument is now the input folder.
    
    Output folder will be the input folder with _ prefixed.
    
    Added usage message.

--- cli.js
+++ cli.js
@@ -13,17 +13,25 @@ const generateIndexInfo = require('./lib/generate-index-info')
 const page = require('./lib/render-page')
 const mdR = require('./lib/markdown-regex')
 
+const [docsFolder, ...argsRest] = process.argv.slice(2)
+
 // Default parameters
-const folder = 'docs'
-const output = '_docs'
+const folder = docsFolder || 'docs'
+const output = `_${folder}`
 const templateFilename = 'template.html'
 const preferences = ['index.md', 'README.md']
 
 // Guards
+// Bail out if more than 1 args
+if (argsRest && argsRest.length > 0) {
+  console.error('Too may arguments')
+  usage(true)
+}
+
 // Bail out if the folder doesn't exist
 if (!sh.test('-e', folder)) {
-  console.error(`Folder ${folder} not found at ${process.cwd()}`)
-  process.exit(1)
+  console.error(`Folder ${folder} not found at ${path.join(process.cwd(), folder)}`)
+  usage(true)
 }
 
 // Define template html, user's first, otherwise default
@@ -60,3 +68,14 @@ mds
     return [f, page(tpl, navHtml, contentHtml)]
   })
   .forEach(([f, p]) => fs.writeFileSync(mdUrl(f), p))
+
+function usage (error) {
+  console.log(`
+Usage:
+
+  markdown-folder-to-html [input-folder]
+
+    input-folder [optional] defaults to \`docs\`
+  `)
+  process.exit(error ? 1 : 0)
+}
